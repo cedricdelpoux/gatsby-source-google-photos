f7836f26a0df4a43994f745c73ffaf9530a5ed2d
Cédric Delpoux <cedric.delpoux@gmail.com>
2020-11-16 07:59:30 +0000

    fix: photo parameters

--- utils/add-photo-url-parameters.js
+++ utils/add-photo-url-parameters.js
@@ -1,17 +1,14 @@
+const numberMax = 16384
 const isSizeValid = (number) =>
-  number && Number.isInteger(number) && number > 0 && number < 16384
+  number && Number.isInteger(number) && number > 0 && number < numberMax
 
 const addPhotoUrlParameters = (url, pluginOptions) => {
-  if (!pluginOptions.photosOptions) {
-    return url
-  }
-
   const {photosMaxWidth, photosMaxHeight, photosCrop} = pluginOptions
   const maxWidth = isSizeValid(photosMaxWidth) && `w${photosMaxWidth}`
   const maxHeight = isSizeValid(photosMaxHeight) && `h${photosMaxHeight}`
   const crop = photosCrop && photosCrop === true && "c"
   const optionsArray = [maxWidth, maxHeight, crop].filter((option) => option)
-  return `${url}${optionsArray.lentgh > 0 ? "=" + optionsArray.join("-") : ""}`
+  return `${url}${optionsArray.length > 0 ? "=" + optionsArray.join("-") : ""}`
 }
 
 module.exports = {
