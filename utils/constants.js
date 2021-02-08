f0ffaa2b2307ef031a31e89b4708c32295310b0c
xylophonehero <68996622+xylophonehero@users.noreply.github.com>
2021-02-08 15:46:35 +0000

    feat: update PAGE_SIZE_PHOTOS from 20 to 10 (#6)

--- utils/constants.js
+++ utils/constants.js
@@ -1,6 +1,6 @@
 module.exports = {
   PAGE_SIZE_ALBUMS: 20,
-  PAGE_SIZE_PHOTOS: 20,
+  PAGE_SIZE_PHOTOS: 10,
   NODE_TYPE_ALBUM: "GooglePhotosAlbum",
   NODE_TYPE_PHOTO: "GooglePhotosPhoto",
   REGEX_MIME_EXT: /^image\/(gif|jpe?g|png|webp)$/,
