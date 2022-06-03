0ac5c9e1b6ca3cfae2e6e71d7289e8c24242f4d1
Cédric Delpoux <cedric.delpoux@gmail.com>
2022-06-03 10:44:09 +0200

    style: remove console.log

--- utils/transform-photo.js
+++ utils/transform-photo.js
@@ -10,12 +10,10 @@ exports.transformPhoto = (photo) => {
 
   // Handle YAML description
   if (photo.description) {
-    console.log("description", photo.description)
     try {
       // Try to convert description from YAML
       const descriptionObject = yamljs.parse(photo.description)
       if (typeof descriptionObject !== "string") {
-        console.log("descriptionObject", descriptionObject)
         Object.assign(photo, descriptionObject)
       }
     } catch (e) {
